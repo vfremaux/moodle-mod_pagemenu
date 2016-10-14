abbbfe73afdc1a537a706b9bd16e6d67fd0a5c49
valery <valery.fremaux@gmail.com>
2016-10-14 12:31:59 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->requires = 2014042900;  // Requires this Moodle version.
 $plugin->component = 'mod_pagemenu';  // Name of component.
 $plugin->maturity = MATURITY_RC;
 $plugin->release = "2.7.0 (Build 2014030800)";
-$plugin->dependencies = array('format_page' => 2013012900);
+$plugin->dependencies = array('format_page' => 2016071203);
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.7.0000';
+$plugin->codeincrement = '2.7.0001';
