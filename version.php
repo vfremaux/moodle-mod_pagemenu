46b7daa8a5672274d871cdd3f1a3551d1e57f39c
valery <valery.fremaux@gmail.com>
2018-05-17 16:15:18 +0200

    fix mod form

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2015111100;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2016052300;  // Requires this Moodle version.
+$plugin->requires = 2017050500;  // Requires this Moodle version.
 $plugin->component = 'mod_pagemenu';  // Name of component.
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "3.2.0 (Build 2014030800)";
+$plugin->release = "3.3.0 (Build 2014030800)";
 $plugin->dependencies = array('format_page' => 2016071203);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0001';
+$plugin->codeincrement = '3.3.0001';
